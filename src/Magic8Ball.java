86eff1a248fb40815f2653ce7e7f0a367ecf7a7a
eyuel <eyuel.negash.2022@student.lhschools.org>
2019-03-08 14:07:50 -0500

    worked at code

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -8,57 +8,57 @@ public class Magic8Ball {
 
         String positive1 = ("Yes");
         String negative1 = ("No");
-        String posative2 = ("Absolutely not");
+        String positive2 = ("Absolutely not");
         String negative2 = ("Absolutely right");
-        String posative3 = ("Most likely");
+        String positive3 = ("Most likely");
         String negative3 = ("Very doubtfull");
-        String posative4 = ("Yes-definitely");
+        String positive4 = ("Yes-definitely");
         String negative4 = ("My source say no");
-        String posative5 = ("Signs point to yes");
+        String positive5 = ("Signs point to yes");
         String negative5 = ("Don't count on it");
 
 
         System.out.println(" ask any question");
         int answers = randomChoice.nextInt();
 
-        if (answers == 10) {
+        if (0<answers&&answers<=10) {
             System.out.println(positive1);
 
 
-        } else if (answers == 20) {
+        } else if (10<answers&&answers <=20) {
 
            System.out.println(negative1);
 
         }
 
-        if (answers == 30) {
-            System.out.println(posative2);
+        if (20<answers&&answers <=30) {
+            System.out.println(positive2);
         }
 
-        else if (answers==40) {
+        else if (30<answers&&answers<=40) {
 
             System.out.println(negative2);
         }
-        if (answers==50) {
+        if (40<answers&&answers<=50) {
 
-            System.out.println(posative3);
+            System.out.println(positive3);
         }
-        else if (answers==60){
+        else if (50<answers&&answers<=60){
             System.out.println(negative3);
         }
-        if (answers==70){
+        if (60<answers&&answers<=70){
 
-            System.out.println(posative4);
+            System.out.println(positive4);
         }
-        else if (answers==80){
+        else if (70<answers&&answers<=80){
 
             System.out.println(negative4);
         }
-        if (answers==90){
+        if (80<answers&&answers<=90){
 
-            System.out.println(posative5);
+            System.out.println(positive5);
         }
-        else if (answers==100){
+        else if (90<answers&&answers<=100){
 
             System.out.println(negative5);
         }
